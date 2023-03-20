51b38ef855fb57ec7b5d0d19454821da37833027
juhay-git <juha.yli-hemminki@seamk.fi>
2023-03-20 10:26:49 +0200

    POST käsittely

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
@@ -8,11 +9,17 @@ lampotilat = [
     {'x':3, 'y':15}
 ]
 
-paivat = ['Maanantai', 'Tiistai', 'Keskiviikko']
+paivat = ['Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai', 'Lauantai', 'Sunnuntai']
 
 @app.route('/api', methods=['GET'])
 def index():
     return render_template("mittaukset.html", taulukko=lampotilat, paivat=paivat)
+
+@app.route('/lisaa', methods=['POST'])
+def lisaa():
+    uusimittaus = request.get_json(force=True)
+    lampotilat.append(uusimittaus)
+    return(json.dumps(uusimittaus))
 
 if __name__ == "__main__":
     app.run(debug=True)
