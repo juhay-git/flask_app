c643c64da490c661831135bd763688cc4ff1e882
juhay-git <juha.yli-hemminki@seamk.fi>
2023-03-27 12:05:35 +0300

    lisätty post lähetys

---
+++ laheta_pyynto.py
@@ -0,0 +1,13 @@
+import json
+import requests
+
+mittaus = {"x": 6,
+           "y" : 10}
+
+while(True):
+    mittaus["x"] = int(input("Anna viikonpäivän numero >"))
+    mittaus["y"] = int(input("Anna päivän lämpötila >"))
+
+    viesti = json.dumps(mittaus)
+    vastaus = requests.post('http://localhost:5000/lisaa', data=viesti)
+
